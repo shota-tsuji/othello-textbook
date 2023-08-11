e2e6fdfdfedcd07b3addd20a68fadf5f7c113d8f
shota-tsuji <shotatuzi@gmail.com>
2023-08-12 07:18:26 +0900

    test: Board creation test with all black discs is prepaired

--- hello_test.cc
+++ hello_test.cc
@@ -1,9 +1,21 @@
 #include <gtest/gtest.h>
+#include "board.hpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-// Expect two strings not to be equal.
-EXPECT_STRNE("hello", "world");
-// Expect equality.
-EXPECT_EQ(7 * 6, 42);
+TEST(BoradTest, Initialization) {
+    int expected[n_board_idx] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 242, 80, 26, 8, 2, 0, 2, 8, 26, 80,
+                                 242, 242, 80, 26, 8, 2, 0, 2, 8, 26, 80, 242};
+    board b;
+    int arr[64] = {0};
+    b.translate_from_arr(arr, 0);
+
+    bool same = true;
+    for (int i = 0; i < n_board_idx; ++i) {
+        if (b.board_idx[i] == expected[i]) {
+            same = false;
+            break;
+        }
+    }
+    EXPECT_TRUE(same);
+    EXPECT_EQ(b.player, 0);
+    EXPECT_EQ(b.n_stones, 64);
 }
