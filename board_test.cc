2488183022a14bc13c632da5f0ad045da1e85549
shota-tsuji <shotatuzi@gmail.com>
2023-08-14 06:30:12 +0900

    test: Move test is added

--- board_test.cc
+++ board_test.cc
@@ -4,13 +4,14 @@
 TEST(BoradTest, Initialization) {
     int expected[n_board_idx] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 242, 80, 26, 8, 2, 0, 2, 8, 26, 80,
                                  242, 242, 80, 26, 8, 2, 0, 2, 8, 26, 80, 242};
-    board b;
+    board_init();
     int arr[64] = {0};
+    board b;
     b.translate_from_arr(arr, 0);
 
     bool same = true;
     for (int i = 0; i < n_board_idx; ++i) {
-        if (b.board_idx[i] == expected[i]) {
+        if (b.board_idx[i] != expected[i]) {
             same = false;
             break;
         }
@@ -48,4 +49,40 @@ TEST(BoradTest, LegalJudge) {
 
     EXPECT_TRUE(!b.legal(0));
     EXPECT_TRUE(b.legal(41));
+}
+
+TEST(BoradTest, EqualsToNextHandWhenMovedFromPreviousHand) {
+    std::vector<int> board_array(hw2, 2);
+    board_array[19] = 0;
+    board_array[27] = 0;
+    board_array[28] = 0;
+    board_array[34] = 1;
+    board_array[35] = 1;
+    board_array[36] = 1;
+
+    board_init();
+    board previous_board;
+    previous_board.translate_from_arr(board_array, 0);
+
+    // e6
+    std::vector<int> next_array(hw2, 2);
+    next_array[19] = 0;
+    next_array[27] = 0;
+    next_array[28] = 0;
+    next_array[34] = 1;
+    next_array[35] = 1;
+    next_array[36] = 0;
+    next_array[44] = 0;
+    board expected;
+    expected.translate_from_arr(next_array, 0);
+    board next = previous_board.move(44);
+
+    bool same = true;
+    for (int i = 0; i < n_board_idx; ++i) {
+        if (next.board_idx[i] != expected.board_idx[i]) {
+            same = false;
+            break;
+        }
+    }
+    EXPECT_TRUE(same);
 }
