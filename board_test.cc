f2d0be07013e4cf3daf53cd7948d54e7f28db848
shota-tsuji <shotatuzi@gmail.com>
2023-08-17 21:55:18 +0900

    test: Test which uses vector is defined

--- board_test.cc
+++ board_test.cc
@@ -21,6 +21,28 @@ TEST(BoradTest, Initialization) {
     EXPECT_EQ(b.n_stones, 64);
 }
 
+TEST(BoradTest, Initialization2) {
+    board_init();
+
+    int expected[n_board_idx] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 242, 80, 26, 8, 2, 0, 2, 8, 26, 80,
+                                 242, 242, 80, 26, 8, 2, 0, 2, 8, 26, 80, 242};
+
+    board b;
+    std::vector<int> board_array(hw2, Black);
+    b.translate_from_arr(board_array, Black);
+
+    bool same = true;
+    for (int i = 0; i < n_board_idx; ++i) {
+        if (b.board_idx[i] != expected[i]) {
+            same = false;
+            break;
+        }
+    }
+    EXPECT_TRUE(same);
+    EXPECT_EQ(b.player, Black);
+    EXPECT_EQ(b.n_stones, hw2);
+}
+
 // 0000000000000000000000000000000000000000000000000000000000000000
 TEST(BoradTest, LegalJudgeAllBlack) {
     board_init();
