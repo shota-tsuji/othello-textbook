ab7cbf9ff52795cf46d47a3f18fd699c84da6137
shota-tsuji <shotatuzi@gmail.com>
2023-08-19 16:13:11 +0900

    test: Evaluation test for d3c5e6 is added

--- tests/evaluation_test.cpp
+++ tests/evaluation_test.cpp
@@ -19,3 +19,11 @@ TEST_F(EvaluationTest, d3c5) {
 
     EXPECT_EQ(0, evaluate(b));
 }
+
+TEST_F(EvaluationTest, d3c5e6) {
+    board b;
+    b.translate_from_arr(TestBoard::d3c5e6(), Black);
+    b.print();
+
+    EXPECT_EQ(-3, evaluate(b));
+}
