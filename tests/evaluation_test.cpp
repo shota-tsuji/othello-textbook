74c792663242ae38316d5aff0df518f77c46567e
shota-tsuji <shotatuzi@gmail.com>
2023-08-19 16:28:46 +0900

    test: Test for opponent evaluation for same boards

--- tests/evaluation_test.cpp
+++ tests/evaluation_test.cpp
@@ -13,25 +13,41 @@ protected:
 };
 
 TEST_F(EvaluationTest, d3c5) {
-    board b;
-    b.translate_from_arr(TestBoard::d3c5(), Black);
-    b.print();
+    board black_d3c5;
+    black_d3c5.translate_from_arr(TestBoard::d3c5(), Black);
+    black_d3c5.print();
 
-    EXPECT_EQ(0, evaluate(b));
+    board white_d3c5;
+    white_d3c5.translate_from_arr(TestBoard::d3c5(), White);
+    white_d3c5.print();
+
+    EXPECT_EQ(0, evaluate(black_d3c5));
+    EXPECT_EQ(0, evaluate(white_d3c5));
 }
 
 TEST_F(EvaluationTest, d3c5e6) {
-    board b;
-    b.translate_from_arr(TestBoard::d3c5e6(), Black);
-    b.print();
+    board black_d3c5e6;
+    black_d3c5e6.translate_from_arr(TestBoard::d3c5e6(), Black);
+    black_d3c5e6.print();
 
-    EXPECT_EQ(-3, evaluate(b));
+
+    board white_d3c5e6;
+    white_d3c5e6.translate_from_arr(TestBoard::d3c5e6(), White);
+    white_d3c5e6.print();
+
+    EXPECT_EQ(-3, evaluate(black_d3c5e6));
+    EXPECT_EQ(3, evaluate(white_d3c5e6));
 }
 
 TEST_F(EvaluationTest, all_black) {
-    board b;
-    b.translate_from_arr(TestBoard::all_black(), Black);
-    b.print();
+    board black_all_black;
+    black_all_black.translate_from_arr(TestBoard::all_black(), Black);
+    black_all_black.print();
 
-    EXPECT_EQ(-104, evaluate(b));
+    board white_all_black;
+    white_all_black.translate_from_arr(TestBoard::all_black(), White);
+    white_all_black.print();
+
+    EXPECT_EQ(-104, evaluate(black_all_black));
+    EXPECT_EQ(104, evaluate(white_all_black));
 }
