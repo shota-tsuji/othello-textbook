e2c8028f6d51f4394b70a66f40c8c2541697aee2
shota-tsuji <shotatuzi@gmail.com>
2023-08-27 11:32:16 +0900

    fix: Substitutions are divided into for-loops

--- board.cpp
+++ board.cpp
@@ -7,17 +7,27 @@ void board_init() {
         for (int place = 0; place < hw; ++place) {
             move_arr[Black][idx][place][0] = move_line_half(b, w, place, 0);
             move_arr[Black][idx][place][1] = move_line_half(b, w, place, 1);
+            move_arr[White][idx][place][0] = move_line_half(w, b, place, 0);
+            move_arr[White][idx][place][1] = move_line_half(w, b, place, 1);
+        }
+    }
+
+    for (int idx = 0; idx < n_line; ++idx) {
+        for (int place = 0; place < hw; ++place) {
             if (move_arr[Black][idx][place][0] || move_arr[Black][idx][place][1])
                 legal_arr[Black][idx][place] = true;
             else
                 legal_arr[Black][idx][place] = false;
-            move_arr[White][idx][place][0] = move_line_half(w, b, place, 0);
-            move_arr[White][idx][place][1] = move_line_half(w, b, place, 1);
             if (move_arr[White][idx][place][0] || move_arr[White][idx][place][1])
                 legal_arr[White][idx][place] = true;
             else
                 legal_arr[White][idx][place] = false;
         }
+    }
+
+    for (int idx = 0; idx < n_line; ++idx) {
+        int b = create_one_color(idx, 0);
+        int w = create_one_color(idx, 1);
         for (int place = 0; place < hw; ++place) {
             flip_arr[Black][idx][place] = idx;
             flip_arr[White][idx][place] = idx;
@@ -33,6 +43,7 @@ void board_init() {
             }
         }
     }
+
     for (int place = 0; place < hw2; ++place) {
         int inc_idx = 0;
         for (int idx = 0; idx < n_board_idx; ++idx) {
@@ -44,6 +55,7 @@ void board_init() {
         if (inc_idx == 3)
             place_included[place][inc_idx] = -1;
     }
+
     for (int idx = 0; idx < n_board_idx; ++idx) {
         for (int place = 0; place < hw2; ++place) {
             local_place[idx][place] = -1;
